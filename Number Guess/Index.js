adcd3dabf8bb905bf3cdde9a1b45a617511cd4dd
codewithimraan <skimran954277@gmail.com>
2025-01-07 14:16:28 +0530

    guess the number project completed

--- Number Guess/Index.js
+++ Number Guess/Index.js
@@ -2,14 +2,34 @@ const min = 1;
 const max = 10;
 const answer = Math.floor(Math.random()*(max-min +1))+min;
 
-let attempts = 0;
+let attempts= 0;
 let guess;
 let running = true;
 
-while (running) {
-    guess = window.prompt('Guess the number betwwen ${min} -${max}');
+while(running){
+    guess = window.prompt(`guess the number between ${min}- ${max}`);
     guess = Number(guess);
-    console.log(guess);
-    
-    
-}e
+
+    if (isNaN(guess)) {
+        window.alert(`please enter correct number`);
+    }   
+    else if (guess < min || guess > max){
+        window.alert(`please enter valid number`);
+    }  
+    else {
+        attempts++;
+        if(guess < answer) {
+        window.alert(`low! try again!`);
+        }
+        else if (guess > answer){
+        window.alert(`High! give it a try`);
+        }
+        else
+        {
+        window.alert(`Wow number ${answer} was right, it took  you ${attempts} attempts`);
+        running= false;
+
+          
+        }
+    }  
+}
